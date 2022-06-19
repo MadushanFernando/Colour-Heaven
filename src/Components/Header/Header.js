f37a07da737a9ab287de418595beaa898ca186c4
thanuja <thanuja.madushan@outlook.com>
2022-06-19 16:41:42 +0530

    logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,9 @@ import "./Header.css";
 function Header(props) {
     return (
         <header className="w-100 d-flex justify-content-center header align-items-center">
-            <div className="w-100 content d-flex justify-content-end">
+            <div className="w-100 content position-absolute d-flex justify-content-end ">
+                <div className="position-absolute logo m-0 p-0">
+                </div>
                 <ul className="nav justify-content-end">
                     <li className="nav-item">
                         <Link className="nav-link fs-16" aria-current="page" to="/">Home</Link>
